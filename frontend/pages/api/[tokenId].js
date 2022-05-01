9c15211d0bf759b318874062745f4275976496c0
t3reetan <tanjunweiterry@gmail.com>
2022-04-30 21:30:05 -0700

    add return keyword

--- frontend/pages/api/[tokenId].js
+++ frontend/pages/api/[tokenId].js
@@ -10,7 +10,7 @@ export default function handler(req, res) {
   // To make our collection compatible with Opensea, we need to follow some Metadata standards (for OpenSea)
   // when sending back the response from the api
   // More info can be found here: https://docs.opensea.io/docs/metadata-standards
-  res.status(200).json({
+  return res.status(200).json({
     name: "Rookie Bot #" + tokenId,
     description:
       "Rookie Bots is a unique NFT collection reppin' rookies in crypto",
